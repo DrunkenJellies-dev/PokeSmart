5bf00c01757a8fd7bcfcdac7ee05e3a44fe5c59a
CallumNewman <ballumnew@gmail.com>
2024-08-03 12:53:06 +0100

    Fixed Comment Editing

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from django.http import messages
+from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm
 
